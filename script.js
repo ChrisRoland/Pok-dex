6c3ef312c03ecc89f7e4a3612768f6b0d2d40a4c
ChrisRoland <chrisebuberoland@gmail.com>
2024-12-29 14:11:22 +0100

    feat: fetch pokemon from API

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
-const cardsContainer = document.querySelector('.cards-container');
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+const container = document.querySelector('.container');
+const search = document.getElementById('search');
+const filter = document.getElementById('filter');
 
-const cards = []
+const pokemons = 200;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemons; i++) {
+        await getPokemon(i);
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `${baseURL}${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    createPokemonCard(data);
+    // console.log(data);
+}
+
+function createPokemonCard(pokemon) {
+    const pokeEl = document.createElement('div');
+    pokeEl.classList.add('pokemon');
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id = pokemon.id.toString().padStart(3, '0');
+    const poke_types = pokemon.types.map(type => type.type.name);
+    const type = mainType(poke_types);
+    const color = colors[type];
+    pokeEl.style.backgroundColor = color;
+    const pokemonInnerHTML = `
+        <div class="img-container">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png" alt="${name}">
+        </div>
+        <div class="info">
+            <span class="number">#${id}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small> <br>
+            <small class="abilities">Abilities: ${pokemon.abilities.map(ability => ability.ability.name).join(', ')}</small>
+            <p>Move: ${pokemon.moves.slice(0, 6).map(move => move.move.name).join(', ')}</p>
+        </div>
+    `;
+    pokeEl.innerHTML = pokemonInnerHTML;
+    container.appendChild(pokeEl);
+}
+
+function mainType(types) {
+    const type = types.find(type => type === 'grass' || type === 'fire' || type === 'water' || type === 'electric' || type === 'ground' || type === 'rock' || type === 'fairy' || type === 'poison' || type === 'bug' || type === 'dragon' || type === 'psychic' || type === 'flying' || type === 'fighting' || type === 'normal');
+    return type;
+}
+
+search.addEventListener('input', filterPokemons);
+
+function filterPokemons(e) {
+    const term = e.target.value;
+    const pokemons = document.querySelectorAll('.pokemon');
+    pokemons.forEach(pokemon => {
+        const name = pokemon.querySelector('.name').innerText;
+        if (name.includes(term)) {
+            pokemon.style.display = 'block';
+        } else {
+            pokemon.style.display = 'none';
+        }
+    });
+}
+
+// fetchPokemons();
